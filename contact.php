7cc9047f24d5d8df7fb7d8a0cd9cb667d81183f6
siphot78 <70804959+siphot78@users.noreply.github.com>
2020-11-23 11:34:06 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,84 @@
+<!Doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
+<link rel="stylesheet" href="sipho's.css" type="text/css">
+<link rel="stylesheet" href="style.css" type="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<title>My Portfolio</title>
+</head>
+<body>
+  <nav class="navbar">
+    <span class="navbar-toggle" id="js-navbar-toggle">
+        <i class="fa fa-navicon" style="font-size:36px"></i>
+    </span>
+    <a href="index.html" class="logo">Sipho Tywele</a>
+    <ul class="main-nav" id="js-menu">
+        <li>
+            <a href="index.html" class="nav-links">Home</a>
+        </li>
+        <li>
+            <a href="project.html" class="nav-links">Projects</a>
+        </li>
+        <li>
+            <a href="about.html" class="nav-links">About</a>
+        </li>
+        <li>
+            <a href="contact.php" class="nav-links">Contact</a>
+        </li>
+       
+    </ul>
+</nav>
+<main>
+
+
+</main>
+
+
+<section id="Contact">
+    <div class="contact container">
+      <div><h1 class="section-title">Contact <span>info</span></h1></div>
+      
+<form class="contact-form" action="contactform.php" method="post">
+<input type="text" name="name" placeholder="your name">
+<input type="text" name="mail" placeholder=" your e-mail">
+<input type="text" name="subject" placeholder="your name">
+<textarea name="message" placeholder="message"></textarea>
+<button type="submit" name="submit">Send</button>
+
+
+</form> 
+      <br><br> <br><br> <br><br> <br><br> <br><br> <br><br> <br><br>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png"/></div>
+          <div class="contact-info">
+            <h1>Phone</h1>
+            <h2>0727973801</h2>
+
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png"/></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>Tywelesipho78@gmail.com</h2>
+
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png"/></div>
+          <div class="contact-info">
+            <h1>Address</h1>
+            <h2>litha,khayelitsha,Cape town</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  </div>
+  <script src="script.js"></script>
+  </body>
+  </html>
